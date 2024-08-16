2f8db981f7ac9787bc6b3d942e5d5f41bfb374e1
杨磊 <905527992@qq.com>
2024-08-16 18:01:12 +0800

    [新增] Package 创建工具和 Package 推送工具.

---
+++ Packages/Kiwi Package/Editor/Packages.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Kiwi.Editor.Package
+{
+	/// <summary>
+	/// Package 工具辅助代码
+	/// </summary>
+	public static class Packages
+	{
+		/// <summary>
+		/// Packages 文件夹路径
+		/// </summary>
+		public static string packagesPath
+		{
+			get
+			{
+				var assetsPath = Application.dataPath;
+
+				return System.IO.Path.Combine(System.IO.Directory.GetParent(assetsPath).FullName , "Packages");
+			}
+		}
+	}
+}
